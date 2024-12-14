3848344928ec51982b37bc77ba93c0ec506934f1
Anders Mousten <anders.mousten@rokoko.com>
2024-12-14 11:55:51 +0100

    Add d11p2

--- src/day11.rs
+++ src/day11.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn read_numbers(file_path: &str) -> Vec<i64> {
     fs::read_to_string(file_path)
@@ -32,9 +33,44 @@ fn part1(file_path: &str) -> i64 {
     stones.len() as i64
 }
 
-fn part2(_file_path: &str) -> i64 {
-    // TODO
-    0
+fn part2(file_path: &str) -> i64 {
+    let stones = read_numbers(file_path);
+    let mut children = HashMap::new();
+    let mut sum = 0;
+    for stone in stones {
+        sum += evolve(stone, 0, 74, &mut children)
+    }
+    sum
+}
+
+fn evolve(stone: i64, i: usize, max: usize, children: &mut HashMap<(i64, usize), i64>) -> i64 {
+    if children.contains_key(&(stone, i)) {
+        return *children.get(&(stone, i)).unwrap();
+    }
+    if stone == 0 {
+        if i == max {
+            return 1;
+        }
+        let result = evolve(1, i+1, max, children);
+        children.insert((stone, i), result);
+        return result;
+    }
+    let str = stone.to_string();
+    let str_len = str.len();
+    if str_len % 2 == 0 {
+        if i == max {
+            return 2;
+        }
+        let result = evolve(str[..str_len/2].parse().unwrap(), i+1, max, children) + evolve(str[str_len/2..].parse().unwrap(), i+1, max, children);
+        children.insert((stone, i), result);
+        return result;
+    }
+    if i == max {
+        return 1;
+    }
+    let result = evolve(stone * 2024, i+1, max, children);
+    children.insert((stone, i), result);
+    result
 }
 
 pub fn run(part: u8, test: bool) -> i64 {
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use simple_stopwatch::Stopwatch;
 
 fn main() {
     let sw = Stopwatch::start_new();
-    println!("Result: {}, spent {} ms", day11::run(1, false), sw.ms());
+    println!("Result: {}, spent {} ms", day11::run(2, false), sw.ms());
 }
